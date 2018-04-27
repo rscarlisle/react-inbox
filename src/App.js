a4d2e146010c58055abb5ba0878de999a219816f
Randall Carlisle <cisolutionsinc@gmail.com>
2018-04-27 15:35:15 -0700

    modified app.ja and app.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ToolbarComponent from './components/ToolbarComponent';
+import MessagesComponent from './components/MessagesComponent';
 
 class App extends Component {
+  state = {
+    messages: this.props.messages
+  };
+
+  toggleProperty = (message, property) => {
+    const index = this.state.messages.indexOf(message);
+    this.setState({
+      messages: [
+        ...this.state.messages.slice(0, index),
+        { ...message, [property]: !message[property] },
+        ...this.state.messages.slice(index + 1)
+      ]
+    });
+  };
+
+  toggleStar = message => {
+    this.toggleProperty(message, 'starred');
+  };
+
+  toggleSelect = message => {
+    this.toggleProperty(message, 'selected');
+  };
+
+  markReadStatus = readStatus => {
+    this.setState({
+      messages: this.state.messages.map(
+        message =>
+          message.selected ? { ...message, read: readStatus } : message
+      )
+    });
+  };
+
+  toggleSelectAll = () => {
+    const selectedMessages = this.state.messages.filter(
+      message => message.selected
+    );
+    const selected = selectedMessages.length !== this.state.messages.length;
+    this.setState({
+      messages: this.state.messages.map(
+        message =>
+          message.selected !== selected ? { ...message, selected } : message
+      )
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ToolbarComponent
+          messages={this.state.messages}
+          toggleSelectAll={this.toggleSelectAll}
+          markReadStatus={this.markReadStatus}
+        />
+        <MessagesComponent
+          messages={this.state.messages}
+          toggleStar={this.toggleStar}
+          toggleSelect={this.toggleSelect}
+        />
       </div>
     );
   }
