afe8570814612fe57c3cba86d5f8bd5df4967543
Randall Carlisle <cisolutionsinc@gmail.com>
2018-04-27 15:32:01 -0700

    edited the config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  // Require your stories here...
-  require('../src/index.css');
-  require('../src/components/MessageComponent.story.js');
-  require('../src/components/MessagesComponent.story.js');
-  require('../src/components/ToolbarComponent.story.js');
-  require('../src/components/ComposeFormComponent.story.js');
-  require('../src/components/InboxPageLayout.story.js');
-  require('../src/components/InboxPage.story.js');
+  require('../stories/ToolbarComponent.story');
 }
+
 configure(loadStories, module);
